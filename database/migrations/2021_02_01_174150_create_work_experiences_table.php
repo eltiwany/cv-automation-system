d87861a21174000cbde00fb4ab78d95edca948d3
lukeloNgajilo <ngajilol@gmail.com>
2021-02-01 20:43:08 +0300

    WorkExperience model created and HobbiesController

---
+++ database/migrations/2021_02_01_174150_create_work_experiences_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_experiences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_experiences');
+    }
+}
