e88943679960fd313abe3732630af1bceb1da78f
lukeloNgajilo <ngajilol@gmail.com>
2021-02-02 00:19:01 +0300

    Referees model updated

--- app/Referees.php
+++ app/Referees.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Referees extends Model
 {
-    //
+    protected $table ='referees';
+    public $primaryKey='id';
+    public $timestamps ='true';
 }
