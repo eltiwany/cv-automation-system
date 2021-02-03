25b65f37609a8898286e4e53d4cdbac465f27cec
lukeloNgajilo <ngajilol@gmail.com>
2021-02-03 20:45:01 +0300

    added language to personal information eloquent relationship

--- app/PersonalInformation.php
+++ app/PersonalInformation.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App;
-
+use app\Language;
 use Illuminate\Database\Eloquent\Model;
 
 class PersonalInformation extends Model
@@ -9,4 +9,14 @@ class PersonalInformation extends Model
 protected $table ='personal_informations';
 public $primaryKey='id';
 public $timestamps ='true';
+
+
+public function Language()
+{
+    return $this->hasMany('app\Language');
+}
+
+
+
+
 }
