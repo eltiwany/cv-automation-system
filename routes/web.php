65b343444cef92e4bae3f71af05d762ef33e3ab4
Ali Saleh <eltiwany@icloud.com>
2021-02-08 16:44:33 +0300

    Add upload image route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('/personal-information' ,'PersonalInformationsController');
 Route::resource('/educational-background' ,'EducationalBackgroundsController');
+Route::post('/upload-image', 'PersonalInformationsController@uploadImage');
