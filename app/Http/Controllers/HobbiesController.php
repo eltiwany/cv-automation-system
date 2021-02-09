57de5db0708fe778ba109fc9bef34969012ede8f
Ed Salema <edwinedward0176@gmail.com>
2021-02-09 15:50:17 +0300

    Changed use App\Hobbies to App\Hobby

--- app/Http/Controllers/HobbiesController.php
+++ app/Http/Controllers/HobbiesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Hobbies;
+use App\Hobby;
 
 class HobbiesController extends Controller
 {
@@ -37,7 +37,7 @@ class HobbiesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $hobbies = new Hobbies
     }
 
     /**
