1228c6bc5da885906617cde2947394285c8ad254
Ed Salema <edwinedward0176@gmail.com>
2021-02-09 15:04:00 +0300

    Added a form

--- resources/views/hobbies.blade.php
+++ resources/views/hobbies.blade.php
@@ -5,14 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Fill in your Hobbies in here....</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                   <form method="" action="">
+
+                      <div>1. <input type="text">
+                      </div>
+                    
+                    </form>
 
                    
                 </div>
