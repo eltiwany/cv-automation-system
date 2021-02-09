7787aa3381394e2f36155cf6f96ebb9bc0329d9b
Ed Salema <edwinedward0176@gmail.com>
2021-02-09 15:27:43 +0300

    Added input types and submit button in the form

--- resources/views/hobbies.blade.php
+++ resources/views/hobbies.blade.php
@@ -8,10 +8,28 @@
                 <div class="card-header">Fill in your Hobbies in here....</div>
 
                 <div class="card-body">
-                   <form method="" action="">
+                @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                   <form action="" method="POST" >
 
-                      <div>1. <input type="text">
-                      </div>
+                      <label for="firsthobbie"> First Hobbie </label>
+                      &nbsp &nbsp &nbsp<input type="text" id="firsthobbie" name="firsthobbie">
+                      <br>
+                      <label for="secondhobbie"> Second Hobbie </label>
+                      <input type="text" id="sechobbie" name="sechobbie">
+                      <br>
+                      <label for="thirdhobbie"> Third Hobbie </label>
+                      &nbsp&nbsp&nbsp<input type="text" id="thirdhobbie" name="thirdhobbie">
+                      <br>
+                      <label for="fourthhobbie"> Fourth Hobbie </label>
+                      &nbsp&nbsp<input type="text" id="fourthhobbie" name="fourthhobbie">
+                      <br>
+                      <input type="submit" value="Submit">
+
+                      
                     
                     </form>
 
