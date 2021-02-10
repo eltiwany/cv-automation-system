27aecc9e689ce6977c63ed1fa91ee08c622fcd01
Ed Salema <edwinedward0176@gmail.com>
2021-02-10 17:03:00 +0300

    Created workexperience.blade.php

---
+++ resources/views/workexperience.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Fill in your Hobbies in here....</div>
+
+                <div class="card-body">
+                @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                   <form action="" method="POST" >
+                    @csrf
+                      <label for="firsthobbie"> List your Hobbies </label>
+                      &nbsp &nbsp &nbsp<input type="textbox" id="firsthobby" size="50" name="firsthobby">
+                      <br>
+                      <label for="secondhobbie"> Second Hobbie </label>
+                      <input type="text" id="sechobby" name="sechobby">
+                      <br>
+                      <label for="thirdhobbie"> Third Hobbie </label>
+                      &nbsp&nbsp&nbsp<input type="text" id="thirdhobby" name="thirdhobby">
+                      <br>
+                      <label for="fourthhobbie"> Fourth Hobbie </label>
+                      &nbsp&nbsp<input type="text" id="fourthhobby" name="fourthhobby">
+                      
+                      <br>
+                      <input type="submit" style="background-color:green" value="Submit">
+
+                      
+                    
+                    </form>
+
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
