425dd6c55d044f57b132d625d54e790f42cbb1da
Ed Salema <edwinedward0176@gmail.com>
2021-02-10 13:44:13 +0300

    Added the projectandresearch.blade.php file in views

---
+++ resources/views/projectandresearch.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Fill in your Hobbies in here....</div>
+
+                <div class="card-body">
+                @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                   <form action="" method="POST" >
+                    @csrf
+                      
+
+                      
+                    
+                    </form>
+
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
