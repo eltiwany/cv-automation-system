f9d5fbb35127ab55da36d9ec6f55925b9047e40e
Ali Saleh <eltiwany@icloud.com>
2021-02-12 12:57:17 +0300

    Add referee index page with table listing

---
+++ resources/views/user-information/referees/index.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header h4 alert-default">
+                    Referees
+                    <a href="/referees/create" class="btn btn-primary btn-sm float-right">
+                        <i class="fa fa-plus"> Add Referee</i>
+                    </a>
+                </div>
+                <div class="card-body">
+                    
+                    @if (count($referees) > 0)
+                        <table class="table table-bordered table-hover">
+                            <tr>
+                                <th>First Name</th>
+                                <th>Other Names</th>
+                                <th>Email</th>
+                                <th>Phone Number</th>
+                                <th>Actions</th>
+                            </tr>
+                            @foreach ($referees as $referee)
+                                <tr>
+                                    <td>{{ $referee->First_Name }}</td>
+                                    <td>{{ $referee->Second_Name }}</td>
+                                    <td>{{ $referee->Email }}</td>
+                                    <td>{{ $referee->PhoneNumber }}</td>
+                                    <td class="text-center">
+                                        <label class="m-0 p-0">
+                                            <a href="/referees/{{ $referee->id }}" class="form-button">
+                                                <button class="btn btn-link text-primary m-0 p-0 pr-3">
+                                                    <i class="fa fa-pencil"></i>
+                                                </button>
+                                            </a>
+                                        </label>
+                                        
+                                        
+                                        <label class="m-0 p-0">
+                                            <form action="/referees/{{ $referee->id }}" method="POST" class="form-button">
+                                                @method('DELETE')
+                                                {{ csrf_field() }}
+
+                                                <button class="btn btn-link text-danger m-0 p-0">
+                                                    <i class="fa fa-trash"></i>
+                                                </button>
+
+                                            </form>
+                                        </label>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    @else
+                        <p class="font-weight-bold text-center p-3">
+                            No referees found, star adding by clicking "Add New" at top left! 
+                        </p>
+                    @endif
+                    {{ $referees->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
