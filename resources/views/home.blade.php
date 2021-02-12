85a3b0a933e9a52da3c3639635eaf5e4088ac8e5
Ali Saleh <eltiwany@icloud.com>
2021-02-12 10:47:04 +0300

    Display icons array on dashboard statistics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,17 +12,22 @@
                     @foreach ($stats as $stat)
                         <?php ++$i; ?>
                         <div class="p-2 col @if ($i == count($stats)) col-md-12 @else col-md-6 @endif m-0">
-                            <div class="alert m-0 @if($stat['percent'] >= 75) alert-success @elseif ($stat['percent'] >= 50) alert-primary @else alert-danger @endif">
-                                <h5 class="card-title">{{ $stat['name'] }}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">{{ $stat['description'] }}</h6>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated  text-dark
-                                    @if($stat['percent'] >= 75) bg-success @elseif ($stat['percent'] >= 50) bg-primary @else bg-danger @endif" 
-                                    role="progressbar" aria-valuenow="{{ $stat['percent'] }}" aria-valuemin="1" aria-valuemax="3" style="width: {{ $stat['percent'] }}%">
-                                        {{ $stat['percent'] }}%
-                                    </div>
-                                </div>  
-                            </div>
+                            <a href="" class="m-0 p-0 no-decoration">
+                                <div class="alert m-0 @if($stat['percent'] >= 75) alert-success @elseif ($stat['percent'] >= 50) alert-primary @else alert-danger @endif">
+                                    <h5 class="card-title">
+                                        <i class="{{ $stat['icon-classes'] }}"></i> 
+                                        {{ $stat['name'] }}
+                                    </h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">{{ $stat['description'] }}</h6>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped progress-bar-animated  text-dark
+                                        @if($stat['percent'] >= 75) bg-success @elseif ($stat['percent'] >= 50) bg-primary @else bg-danger @endif" 
+                                        role="progressbar" aria-valuenow="{{ $stat['percent'] }}" aria-valuemin="1" aria-valuemax="3" style="width: {{ $stat['percent'] }}%">
+                                            {{ $stat['percent'] }}%
+                                        </div>
+                                    </div>  
+                                </div>
+                            </a>
                         </div>
                     @endforeach
                     </div>
