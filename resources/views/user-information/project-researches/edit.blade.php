883b77c52d822f9dff78f505f6bbc2b8b6f5c3ac
Ed Salema <edwinedward0176@gmail.com>
2021-02-16 13:33:55 +0300

    Added css syling to project-researches edit  blade file

--- resources/views/user-information/project-researches/edit.blade.php
+++ resources/views/user-information/project-researches/edit.blade.php
@@ -4,11 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12 p-0">
-            <div class="card m-0">
-                <div class="card-header h4 alert-default">
-                    Update Information for Project and Researches: {{ $pandr->First_Name . " "  . $pandr->Second_Name}}
+        <div class="card shadow-custom">
+                <div class="card-header h4 bg-custom-medium">
+                    <i class="fa fa-check-circle header-icon"></i>
+                    Update Information for Project and Researches: {{ $pandr->name . " "  }}
                 </div>
-                <div class="card-body">
+                <div class="card-body bg-custom-light">
                     <form action="/project-researches/{{ $pandr->id }}" method="POST">
                         @method('PUT')
                         @csrf
@@ -20,12 +21,12 @@
                             
                             <div class="form-group col-md-6">
                                 <label for="TimeSta">Other Names</label>
-                                <input type="text" name="time_started" value="{{ $pandr->time_started }}" id="time_started" placeholder="time_started" class="form-control"/>
+                                <input type="text" name="time_started" value="{{ $pandr->TimeStarted }}" id="time_started" placeholder="time_started" class="form-control"/>
                             </div> 
 
                             <div class="form-group col-md-6">
                                 <label for="phone_number">Phone Number</label>
-                                <input type="text" name="time_ended" value="{{ $pandr->time_ended}}" id="time_ended" placeholder="time_ended" class="form-control"/>
+                                <input type="text" name="time_ended" value="{{ $pandr->TimeEnded}}" id="time_ended" placeholder="time_ended" class="form-control"/>
                             </div> 
 
                             
