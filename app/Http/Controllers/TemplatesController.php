bf921df32275d05fcd5091635c484f191ef04eee
Ali Saleh <eltiwany@icloud.com>
2021-02-17 20:54:11 +0300

    Added create user template functions

--- app/Http/Controllers/TemplatesController.php
+++ app/Http/Controllers/TemplatesController.php
@@ -9,8 +9,10 @@ use App\PersonalInformation;
 use App\ProjectAndResearch;
 use App\Referee;
 use App\Template;
+use App\UserTemplate;
 use App\WorkExperience;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TemplatesController extends Controller
 {
@@ -72,7 +74,43 @@ class TemplatesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Validator::make($request->all(), [
+            'name' => 'required',
+            'heading_class' => 'required',
+            'body_class' => 'required'
+        ])->validate();
+
+        if (!(UserTemplate::where([
+                'name' => $request->get('name'),
+                'selected_heading_class' => $request->get('heading_class'),
+                'selected_body_class' => $request->get('body_class')
+            ])->exists())) {
+                UserTemplate::where([
+                    'user_id' => auth()->user()->id
+                ])->update(['is_selected' => false]);
+
+                $user_template = new UserTemplate;
+                $user_template->name = $request->get('name');
+                $user_template->is_selected = true;
+                $user_template->selected_heading_class = $request->get('heading_class');
+                $user_template->selected_body_class = $request->get('body_class');
+                $user_template->user_id = auth()->user()->id;
+                $user_template->save();
+        }else {
+            UserTemplate::where([
+                'user_id' => auth()->user()->id
+            ])->update(['is_selected' => false]);
+
+            $$user_template = UserTemplate::where([
+                'name' => $request->get('name'),
+                'selected_heading_class' => $request->get('heading_class'),
+                'selected_body_class' => $request->get('body_class')
+            ])->first();
+            $user_template->is_selected = true;
+        }
+
+        return redirect()->route('user-templates.index')->with('success', 'Template has been saved.');
+
     }
 
     /**
