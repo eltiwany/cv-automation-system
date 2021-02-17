36d10c823efe9144a47bb13f29a685b19b7e0fa0
Ali Saleh <eltiwany@icloud.com>
2021-02-17 14:50:23 +0300

    Added create template form with choices of headers and body

---
+++ resources/views/templates/create.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 p-0">
+            <div class="card shadow-custom">
+                <div class="card-header h4 bg-custom-medium">
+                    <i class="fa fa-plus header-icon"></i>
+                    Create Template 
+                </div>
+                <div class="card-body bg-custom-light mb-5">
+                    <form action="/templates" method="POST" class="m-0 p-0 row">
+                        <div class="alert alert-info col-md-12">
+                            <div class="fa fa-slack"></div> 
+                            Select Heading Style
+                        </div>
+                        @foreach ($templates as $template)
+                            <div class="col-md-6">
+                                <div class="shadow-custom-lg">
+                                    <div class="template-enclosure-header p-3">
+                                        <input type="radio" name="header_class" value="{{ $template->header_class }}" id="header_class{{ $template->id }}">
+                                        <label for="header_class{{ $template->id }}">{{ $template->name }} </label>
+                                        @include('templates.sample-header')
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                        <div class="m-3"></div>
+                        <div class="alert alert-info col-md-12">
+                            <div class="fa fa-slack"></div> 
+                            Select Body Style
+                        </div>
+                        @foreach ($templates as $template)
+                            <div class="col-md-6">
+                                <div class="shadow-custom-lg">
+                                    <div class="template-enclosure-body p-3">
+                                        <input type="radio" name="body_class" value="{{ $template->body_class }}" id="body_class{{ $template->id }}">
+                                        <label for="body_class{{ $template->id }}">{{ $template->name }} </label>
+                                        @include('templates.sample-body')
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+
+                        <button class="btn btn-primary btn-block m-3">
+                            <i class="fa fa-plus"></i> 
+                            Create Template
+                        </button>
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
