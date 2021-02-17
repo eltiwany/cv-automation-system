9ac779341fca0814ccbdc3e98a9f477852b431c1
jonathanmoyo <jonathanmoyo123@gmail.com>
2021-02-17 11:42:41 -0800

    Added layout file for printing with no headers or footers

---
+++ resources/views/layouts/print-app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Cv Automation') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/custom-side.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/custom.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <?php 
+        $full_url = url()->current();
+        $break_url = explode('/', $full_url);
+        $url = end($break_url);
+    ?>
+</head>
+<body>  
+    @if (!auth()->guest())
+        @yield('content')  
+    @endif
+    <!-- page-wrapper -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/custom-side.js') }}"></script>
+</body>
+</html>
