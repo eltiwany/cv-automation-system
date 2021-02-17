6e6c21a03291ea646ec8c7b69fd192dfebca6c0b
jonathanmoyo <jonathanmoyo123@gmail.com>
2021-02-17 11:47:48 -0800

    Designing heading section to print file

--- resources/views/download-print/print.blade.php
+++ resources/views/download-print/print.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.print-app')
 
 @section('content')
-    
+<div class="container-print">
+    <div class="custom-paper m-auto" id="cv">
+        <div class="header-paper1 p-3">
+            <div class="img-profile">
+                @if (!empty(auth()->user()->logo_url))
+                    <p>    
+                        <img src="{{ asset('storage/profile_images/' . auth()->user()->logo_url) }}"> 
+                    </p>
+                @else
+                    <p>
+                        <img src="{{ asset('storage/profile_images/default.png') }}"> 
+                    </p>
+                @endif
+            </div>
+            <div class="heading">
+                @if ($user_exist)
+                    <h2>{{ strtoupper(auth()->user()->first_name . " " . auth()->user()->middle_name . " " . auth()->user()->last_name) }}</h2>
+                    <p>{{ $personal_information->Email }} | {{ $personal_information->Phone_Number }} | {{ $personal_information->Address }}</p>
+                @else
+                    <h2>Full Name</h2>
+                    <p>Email | Number | Address</p>
+                @endif
+            </div>
+        </div>
+    </div>
 @endsection
