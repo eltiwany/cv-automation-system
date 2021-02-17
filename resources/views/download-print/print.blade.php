9b19282ba9864b62c3a180a6b83c7ced3fe9a0d7
jonathanmoyo <jonathanmoyo123@gmail.com>
2021-02-17 11:48:35 -0800

    Designing body section with summary information

--- resources/views/download-print/print.blade.php
+++ resources/views/download-print/print.blade.php
@@ -26,4 +26,17 @@
             </div>
         </div>
     </div>
+
+    <div class="body-paper1">
+
+        {{-- Summary about yourself --}}
+        <div class="section">
+            <div class="body-heading">
+                <h4>Summary</h4>
+                <div class="box-hidden">Summary</div>
+                <div class="border-center"></div>
+            </div>
+            <p class="full-width">A summary is a brief statement or restatement of main points, especially as a conclusion to a work: a summary of a chapter. A brief is a detailed outline, by heads ...</p>
+        </div>
+    </div>
 @endsection
