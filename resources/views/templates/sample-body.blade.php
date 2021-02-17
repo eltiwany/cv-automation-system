f5ea5cde7f023423cb44db7dadbe9556fdd2aff2
Ali Saleh <eltiwany@icloud.com>
2021-02-17 20:57:13 +0300

    Added file to preview only body of a template

---
+++ resources/views/templates/sample-body.blade.php
@@ -0,0 +1,14 @@
+<div class="m-auto" id="cv">
+    <div class="{{ $template['body_class'] }}">
+        {{-- Summary about yourself --}}
+        <div class="section">
+            <div class="body-heading">
+                <h4>Summary</h4>
+                <div class="box-hidden">Summary</div>
+                <div class="border-center"></div>
+            </div>
+            <p class="full-width">A summary is a brief statement or restatement of main points, especially as a conclusion to a work: a summary of a chapter. A brief is a detailed outline, by heads ...</p>
+        </div>
+
+    </div>
+</div>
