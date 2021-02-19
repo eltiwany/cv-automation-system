572d7524f6c16925487f3d11233cefcee13472c9
Ali Saleh <eltiwany@icloud.com>
2021-02-19 20:47:13 +0300

    Added authentication middleware to contructor of TemplatesController

--- app/Http/Controllers/TemplatesController.php
+++ app/Http/Controllers/TemplatesController.php
@@ -17,6 +17,15 @@ use Illuminate\Support\Facades\Validator;
 class TemplatesController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
