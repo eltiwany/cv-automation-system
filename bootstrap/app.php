89eda604ab4421b5a8bc1b65e7057e594ff15a7d
jonathanmoyo <jonathanmoyo123@gmail.com>
2021-02-19 05:47:09 -0800

    Fixed Config Confict of domPDF provider in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,7 +41,7 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$app->register(\Barryvdh\DomPDF\ServiceProvider::class);
+// $app->register(\Barryvdh\DomPDF\ServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
